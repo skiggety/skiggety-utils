592738b990e5c1377b869757bb8b5f7597241902
Scott Longley <skiggety@gmail.com>
2020-09-30 09:17:51 -0400

    add .bash extension to bashrc include file

--- installers/skiggety.bash.config/bashrc_section.bash
+++ installers/skiggety.bash.config/bashrc_section.bash
@@ -3,4 +3,4 @@
 # "$SKIGGETY_UTILS_DIR/installers/skiggety.bash.config/bashrc_section.bash", # FROM_SKIGGETY_UTILS
 # this should be installed into the user's .bashrc file:                     # FROM_SKIGGETY_UTILS
 export SKIGGETY_UTILS_DIR="$HARDCODED_SKIGGETY_UTILS_DIR"                    # FROM_SKIGGETY_UTILS
-. $SKIGGETY_UTILS_DIR/lib/include_in_bashrc                                  # FROM_SKIGGETY_UTILS
+. $SKIGGETY_UTILS_DIR/lib/include_in_bashrc.bash                             # FROM_SKIGGETY_UTILS
