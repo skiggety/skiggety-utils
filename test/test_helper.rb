8bb26dab9e505c3de586725dff9f5e4bf55dacfb
Scott Longley <skiggety@gmail.com>
2020-10-01 16:38:14 -0400

    progress bar for unit tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ require "minitest/autorun"
 require 'minitest/reporters'
 require 'simplecov'
 
-Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(:color => true)]
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new(:color => true)]
 
 SimpleCov.start
 # TODO: follow https://github.com/simplecov-ruby/simplecov/issues/926 for a new version of ximplecov that won't complain about @enable_for_subprocesses not being initialized
