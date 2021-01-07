7aa9ffaf9807ddaa39f0ff13ad5153d168a3ea3f
Scott Longley <skiggety@gmail.com>
2021-01-07 12:49:00 -0500

    set up this repo for python 3 with pyenv

--- lib/include_in_bashrc.bash
+++ lib/include_in_bashrc.bash
@@ -17,3 +17,9 @@ trap 'if [ "login" == "$(ps -o comm= $PPID)" ];then echo holding the window open
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
 
+# pyenv stuff (should be at the bottom of this file):
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+ eval "$(pyenv init -)"
+fi
