fd03bedcae28d0ee926e1cf6a34b749974425de7
Scott Longley <skiggety@gmail.com>
2021-01-18 09:47:50 -0500

    python import another file

---
+++ lib/lines.py
@@ -0,0 +1,9 @@
+
+# TODO TODO TODO TODO TODO TODO: TDD
+# TODO TODO TODO TODO TODO: IMPLEMENT
+  # lines
+    # source location
+    # text
+    # processed_text
+    # replace in original file somehow (perhaps can't use external grep)
+
