d8265a1ffc89e5af59730aa536149d93593a9b56
Scott Longley <skiggety@gmail.com>
2021-05-21 01:22:05 -0400

    new version of todo works, but not as well as the old one

--- lib/todo_line.py
+++ lib/todo_line.py
@@ -1,3 +1,4 @@
+import re # regular expressions
 
 # TODO TODO TODO TODO TODO: IMPLEMENT
   # TodoLine
@@ -7,13 +8,27 @@
     # replace in original file somehow (perhaps can't use external grep)
 
 class TodoLine:
-    def text():
-        return("TODO TODO: ...") # TODO: IMPLEMENT
+    def __init__(self, grep_output_text):
+        self.file_path, self.line_number, self.original_text = '', '', ''
+        match = re.search(r'^(.*?):(\d*):(.*)$', grep_output_text)
+        if(match):
+            self.file_path, self.line_number, self.original_text = match.group(1), match.group(2), match.group(3)
+        else:
+            raise Exception("Can't create a TodoLine from this--TOOD: change this to avoid using this exception for control flow")
+        # TODO filter for Binary files
+        # TODO separate into public fileds: file_path, line_number, original_text
+        # TODO filter for IGNORE_TODO
 
-    def processed_text():
-        return("TODO^2: ...") # TODO: IMPLEMENT
+    def __str__(self):
+        # TODO: print it out with full grep output
+        return self.file_path + ":" + self.line_number + ":" + self.text
 
-    def power():
+    @property
+    def text(self):
+        return(self.original_text) # TODO: IMPLEMENT for real
+
+    @property
+    def votecount():
         return(2) # TODO: IMPLEMENT
 
     def write():
