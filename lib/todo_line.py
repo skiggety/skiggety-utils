29f9b29c280f7e2c8fa27d970cc1e40782dc9471
Scott Longley <skiggety@gmail.com>
2021-05-21 21:52:41 -0400

    filter out IGNORE_TODO

--- lib/todo_line.py
+++ lib/todo_line.py
@@ -13,8 +13,10 @@ class TodoLine:
         match = re.search(r'^(.*?):(\d*):(.*)$', grep_output_text)
         if(match):
             self.file_path, self.line_number, self.original_text = match.group(1), match.group(2), match.group(3)
+            if re.search(r'IGNORE_TODO', self.original_text):
+                raise Exception("Can't create a TodoLine from this--TODO: change this to avoid using this exception for control flow")
         else:
-            raise Exception("Can't create a TodoLine from this--TOOD: change this to avoid using this exception for control flow")
+            raise Exception("Can't create a TodoLine from this--TODO: change this to avoid using this exception for control flow")
         # TODO filter for Binary files
         # TODO separate into public fileds: file_path, line_number, original_text
         # TODO filter for IGNORE_TODO
