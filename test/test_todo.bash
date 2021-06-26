98e200794d1e69cc2a9b58709b46e51f477afcf3
Scott Longley <skiggety@gmail.com>
2021-06-26 07:59:28 -0400

    replace bin/todo with bin/todo-new now that it is ready

--- test/test_todo.bash
+++ test/test_todo.bash
@@ -13,14 +13,11 @@ function main {
     test_todo_shows_nothing_from_empty_file || exit_with_error "test failed"
     test_todo_sorts_two_equivalent_lines_by_line_number_and_exits_correctly || exit_with_error "test failed"
     test_todo_handles_a_complex_example || exit_with_error "test failed"
-
-    # If all the tests pass, use the new program as the real TODO:
-    # TODO TODO TODO: cp "$(which todo-new)" "$(which todo)"
 }
 
 function test_todo_shows_nothing_from_empty_dir {
     pushd "$todo_examples_dir/empty_dir" > /dev/null
-    result="$(todo-new -k TXDX)"
+    result="$(todo -k TXDX)"
     exit_value="$?"
     assert_equal "" "$result"
     assert_equal "0" "$exit_value"
@@ -29,7 +26,7 @@ function test_todo_shows_nothing_from_empty_dir {
 
 function test_todo_shows_nothing_from_empty_file {
     pushd "$todo_examples_dir/empty_file_dir" > /dev/null
-    result="$(todo-new -k TXDX)"
+    result="$(todo -k TXDX)"
     exit_value="$?"
     assert_equal "" "$result"
     assert_equal "0" "$exit_value"
@@ -38,7 +35,7 @@ function test_todo_shows_nothing_from_empty_file {
 
 function test_todo_sorts_two_equivalent_lines_by_line_number_and_exits_correctly {
     pushd "$todo_examples_dir/simple" > /dev/null
-    result="$(todo-new -k TXDX)"
+    result="$(todo -k TXDX)"
     exit_value="$?"
     expected="./example.txt:1:TXDX: this is a todo
 ./example.txt:2:  TXDX: this is another todo that's indented"
@@ -49,7 +46,7 @@ function test_todo_sorts_two_equivalent_lines_by_line_number_and_exits_correctly
 
 function test_todo_handles_a_complex_example {
     pushd "$todo_examples_dir" > /dev/null
-    result="$(todo-new -k TXDX)"
+    result="$(todo -k TXDX)"
     exit_value="$?"
     expected="./simple/example.txt:1:TXDX: this is a todo
 ./simple/example.txt:2:  TXDX: this is another todo that's indented
