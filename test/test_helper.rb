387b416fcdcabf3848fbd63e7bcbe37a44f49357
Scott Longley <skiggety@gmail.com>
2021-09-28 06:28:18 -0400

    shorten test output

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,9 @@
 
 # make sure symlinks exist to include installers in test coverage
 link_dir = File.expand_path(File.join(__dir__, '../lib/coverage_links'))
-puts "link_dir is \"#{link_dir}\""
+# puts "link_dir is \"#{link_dir}\""
 installer_dir = File.expand_path(File.join(__dir__, '../installers'))
-puts "installer_dir is \"#{installer_dir}\""
+# puts "installer_dir is \"#{installer_dir}\""
 Dir.glob(File.join(installer_dir, '*')).each do |installer|
   next unless File.file?(installer)
   next unless system("head -n 1 #{installer} | grep ruby > /dev/null")
