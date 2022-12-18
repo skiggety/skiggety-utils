641e7de74f7aed63e9608889cbbd2536be7f021b
Scott Longley <skiggety@gmail.com>
2022-12-18 07:16:53 -0500

    committing a script specifically to debug CFPVOM bug, so it's easy to run on different computers

---
+++ PWD_BIN/debug_CFPVOM.bash
@@ -0,0 +1,4 @@
+
+# TODO^31: just a way to control where and how and how often to run demo_python_version_hack & demo_ruby_version_hack to do whatever testing we need to . And control where the output goes, etc.
+# TODO^31: call this from your local PWD/experiment.gitignored.bash on all machines that want to
+# participate in debugging.
