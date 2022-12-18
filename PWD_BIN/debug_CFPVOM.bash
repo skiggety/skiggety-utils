eb66d9393fe37b2323e026349748f0cf76d986c7
Scott Longley <skiggety@gmail.com>
2022-12-18 09:45:16 -0500

    capture output from debug_CFPVOM.bash

--- PWD_BIN/debug_CFPVOM.bash
+++ PWD_BIN/debug_CFPVOM.bash
@@ -9,7 +9,6 @@ SKIGGETY_DEBUG=true
 # TODO^31: call this from your local PWD/experiment.gitignored.bash on all machines that want to
 # participate in debugging.
 
-
 # go where there's no accurate .python-version to rely on to put these version hacks to the test:
 cd ~/today_todo/
 
@@ -17,12 +16,15 @@ for cmd in demo_python_version_hack \
            demo_ruby_version_hack
 do
     # TODO: try ask_user congrats, you are in a debugger at TODO_current_script_name_and_line_number_TODO
-    OUTFILE=$cmd.gitignored.output.txt
+    OUTFILE=$SKIGGETY_UTILS_DIR/logs/$cmd.log.$(whoami).at.$(hostname).txt
+    mkdir -p "$(dirname "$OUTFILE")"
     echo "at: $(duh)" | tee $OUTFILE
-    echo "running: '$cmd'" | tee $OUTFILE
-    $cmd | tee -a $OUTFILE || exit_with_error "$cmd FAILED"
-    echo_debug "your test results are in $OUTFILE TODO^33 no they are not"
-    exit # TODO: DELETE when it's time to play with other versions
+    echo "running: '$cmd'" | tee -a $OUTFILE
+    $cmd | tee -a $OUTFILE || accumulate_error "$cmd FAILED"
+    echo "your test results are in $OUTFILE"
+
+    exit_with_error "stopping now: one language at a time TODO: DELETE when the python hack is working everywhere"
+    exit_if_any_accumulated_errors
 done
 
 # TODO: DELETE this script when done
