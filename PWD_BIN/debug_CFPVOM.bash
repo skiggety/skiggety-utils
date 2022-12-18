e666a1373c41cd8346a70c4dafa1095e8d4ad77c
Scott Longley <skiggety@gmail.com>
2022-12-18 08:50:13 -0500

    debuggin the debug script

--- PWD_BIN/debug_CFPVOM.bash
+++ PWD_BIN/debug_CFPVOM.bash
@@ -9,19 +9,19 @@ SKIGGETY_DEBUG=true
 # TODO^31: call this from your local PWD/experiment.gitignored.bash on all machines that want to
 # participate in debugging.
 
-# TODO^31: ask_user "debug the rest of $0"
-exit
 
 # go where there's no accurate .python-version to rely on to put these version hacks to the test:
-cd ~/today_todo/; duh # to make it easier to diff output on two machines
+cd ~/today_todo/
+
 for cmd in demo_python_version_hack \
            demo_ruby_version_hack
 do
-    # XXX TODO^31: tee everything in here:
     # TODO: try ask_user congrats, you are in a debugger at TODO_current_script_name_and_line_number_TODO
-    debug_here "running: '$cmd'"
-    # XXX TODO^27: learn to use the damned vim copy/paste buffers better for lines like this:
-    $cmd >$cmd.gitignored.output.txt || exit_with_error "$cmd FAILED"
+    OUTFILE=$cmd.gitignored.output.txt
+    echo "at: $(duh)" | tee $OUTFILE
+    echo "running: '$cmd'" | tee $OUTFILE
+    $cmd | tee -a $OUTFILE || exit_with_error "$cmd FAILED"
+    echo_debug "your test results are in $OUTFILE TODO^33 no they are not"
 done
 
 # TODO: DELETE this script when done
