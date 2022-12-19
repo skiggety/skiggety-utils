164741fa9ddc8ff89e8f4c38ee109af8149b4d36
Scott Longley <skiggety@gmail.com>
2022-12-19 03:46:29 -0500

    cleanup

--- PWD_BIN/debug_CFPVOM.bash
+++ PWD_BIN/debug_CFPVOM.bash
@@ -8,7 +8,6 @@
 # TODO^31: call this from your local PWD/experiment.gitignored.bash on all machines that want to
 # participate in debugging.
 
-
 # go where there's no accurate .python-version to rely on to put these version hacks to the test:
 cd ~/today_todo/
 
