99c60d4c014d2fbd3627f8829862c7329293a0b5
Scott Longley <skiggety@gmail.com>
2023-01-25 07:07:03 -0500

    getting ready to have some development help: voting, renaming

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,15 +6,27 @@ FileUtils.mkdir_p link_dir
 # puts "link_dir is \"#{link_dir}\""
 installer_dir = File.expand_path(File.join(__dir__, '../installers'))
 # puts "installer_dir is \"#{installer_dir}\""
+
+necessary_symlinks = []
+# Create necessary_symlinks for code coverage
 Dir.glob(File.join(installer_dir, '*')).each do |installer|
   next unless File.file?(installer)
   next unless system("head -n 1 #{installer} | grep ruby > /dev/null")
 
   installer_link = File.join(link_dir, File.basename(installer) + '.rb')
   installer_relpath = File.join('../../installers', File.basename(installer))
+  necessary_symlinks << installer_link
   unless File.exist?(installer_link)
     puts "CREATING SYMLINK: #{installer_link} --> #{installer_relpath}"
     File.symlink(installer_relpath, installer_link)
+  end
+end
+
+# Remove dead links
+Dir.glob(File.join(link_dir, '*')).each do |found_symlink|
+  unless necessary_symlinks.include?(found_symlink)
+    puts "DELETING SYMLINK: #{found_symlink}"
+    File.delete(found_symlink)
   end
 end
 
--- todo_report/README.txt
+++ todo_report/README.txt
@@ -1,7 +1,7 @@
 ======================================================================================================== TODOs, ascending. Do not edit, run 'generate_todo_report' to generate. 
     ( showing last 5 lines out of 411: )
-./README.md:44:    - TODO^7: by the way, figure out how to tack one git repo on to another so you don't have ./PWD_BIN always showing up in the
-./installers/tmux:41:      # TODO^7: maybe use shellask, but don't assume user is ready to be launched into vim:
+./installers/vim:82:      # TODO^7: maybe use shellask, but don't assume user is ready to be launched into vim. Even though you are
+./installers/tmux:41:      # TODO^8: maybe use shellask, but don't assume user is ready to be launched into vim:
 ./README.md:134:  - TODO^10: precommit to prevent committing X X X (without the spaces)
 ./README.md:117:  - TODO^11: ...rewrite todo in rust with parallelization (with Kevin)
 ./README.md:116:  - TODO^15: do [rust koans](https://github.com/crazymykl/rust-koans) (with Kevin) so we can prepare to...
