420d8bdac8b08f31c60b699b692bfcac39690c74
Scott Longley <slongley@eezy.com>
2024-04-08 08:20:18 -0400

    reword mute notice for firstlife

--- lib/include_in_bashrc.bash
+++ lib/include_in_bashrc.bash
@@ -12,6 +12,9 @@ export EDITOR
 # ./PWD_BIN comes before $SKIGGETY_UTILS_DIR/bin so that you can override scripts like "dashboard"
 export PATH="$PATH:./PWD_BIN:$SKIGGETY_UTILS_DIR/bin:$HOME/bin"
 
+# TODO^116: (IN_PROGRESS, NOW): fold in zerothlife config:
+# EASY TODO^114: should firstlife/bin/ be in the path before skiggety_utils so you can override firstlife code?
+
 # this weird way of getting the git branch works even when not in a git repo:
 # TODO: extract the part that shows the git branch?:
 export PS1="\$(duh)\$(git branch 2>/dev/null|grep '^*'|sed 's/^\\*/ \\*/') - \D{%F %T}\n\$ " # TODO: add colors (besides duh)
