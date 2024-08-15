fec95e0c2c4bbe14bbf08bb5b91cd5224b6deb13
Scott Longley <skiggety@tinhat.local>
2024-08-14 21:53:51 -0400

    new colors

--- lib/skiggety-utils.bash
+++ lib/skiggety-utils.bash
@@ -7,14 +7,16 @@
 
 red='\033[0;31m'
 RED='\033[1;31m'
+blinky_RED='\033[1;5;31m'
 green='\033[0;32m'
 GREEN='\033[1;32m'
 yellow='\033[0;33m'
 YELLOW='\033[1;33m'
 cyan='\033[0;36m'
 CYAN='\033[1;36m'
-blinky_CYAN='\033[5;36m'
+blinky_CYAN='\033[1;5;36m'
 no_color='\033[0m'
+
 newline='
 '
 
