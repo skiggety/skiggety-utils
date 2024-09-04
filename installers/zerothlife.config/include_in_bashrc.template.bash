55740b1933b94c7ff9a3de4f6b253ab53a2857a9
Scott Longley <slongley@eezy.com>
2024-09-03 21:43:52 -0400

    ziggy!!

--- installers/zerothlife.config/include_in_bashrc.template.bash
+++ installers/zerothlife.config/include_in_bashrc.template.bash
@@ -2,3 +2,5 @@ export PATH="$ZEROTHLIFE_DIR/bin:$ZEROTHLIFE_CODE_DIR/bin:$PATH"
 
 alias cdzl="cd $ZEROTHLIFE_DIR"
 alias cdzlc="cd $ZEROTHLIFE_CODE_DIR"
+alias ziggy="zl-gamify && echo y && exit 0"
+alias zgy="zl-gamify && echo y && exit 0"
