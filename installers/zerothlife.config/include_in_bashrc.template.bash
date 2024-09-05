b1fafb9a3a86272d7126e0628b12503dacdc2177
Scott Longley <skiggety@tinhat.local>
2024-09-05 08:24:40 -0400

    'effit' and 'forkit'

--- installers/zerothlife.config/include_in_bashrc.template.bash
+++ installers/zerothlife.config/include_in_bashrc.template.bash
@@ -3,6 +3,8 @@ export PATH="$ZEROTHLIFE_DIR/bin:$ZEROTHLIFE_CODE_DIR/bin:$PATH"
 alias cdzl="cd $ZEROTHLIFE_DIR"
 alias cdzlc="cd $ZEROTHLIFE_CODE_DIR"
 
+alias forkit="zl-fork-delegate  && echo y && exit 0"
+alias effit="zl-fork-delegate && echo y && exit 0"
 alias effin_ziggy="zl-fork-gamify && echo y && exit 0"
 alias ziggy="zl-gamify && echo y && exit 0"
 alias fzgy="zl-fork-gamify && echo y && exit 0"
