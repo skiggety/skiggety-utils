fef6e5d1ddc564eb14f4f429e23693bae33fbcb5
Scott Longley <skiggety@tinhat.local>
2024-09-07 09:09:03 -0400

    I think we're good with that testing..

--- installers/zerothlife.config/include_in_bashrc.template.bash
+++ installers/zerothlife.config/include_in_bashrc.template.bash
@@ -5,10 +5,13 @@ alias cdzlc="cd $ZEROTHLIFE_CODE_DIR"
 
 alias forkit="zl-fork-delegate  && echo y && exit 0"
 alias effit="zl-fork-delegate && echo y && exit 0"
-alias effin_ziggy="zl-fork-gamify && echo y && exit 0"
-alias ziggy="zl-gamify && echo y && exit 0"
-alias omg-ziggy="zl-gamify --urgent && echo y && exit 0"
-alias zaggy="zl-gamify --urgent && echo y && exit 0"
-alias zuggy="zl-gamify --urgent && echo y && exit 0"
-alias fzgy="zl-fork-gamify && echo y && exit 0"
+
 alias zgy="zl-gamify && echo y && exit 0"
+alias ziggy="zl-gamify && echo y && exit 0"
+
+alias fzgy="zl-fork-gamify && echo y && exit 0"
+alias effin_ziggy="zl-fork-gamify && echo y && exit 0"
+
+alias zaggy="zl-gamify --urgent && echo y && exit 0"
+alias omg-ziggy="zl-gamify --urgent && echo y && exit 0"
+alias zuggy="zl-gamify --urgent && echo y && exit 0"
