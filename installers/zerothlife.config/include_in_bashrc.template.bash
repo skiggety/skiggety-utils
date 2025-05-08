b158530d30eb4d567fb0443be2f4ea95330f9eda
Scott Longley <skiggety@tinhat.local>
2025-05-08 12:17:09 -0400

    cleanup

--- installers/zerothlife.config/include_in_bashrc.template.bash
+++ installers/zerothlife.config/include_in_bashrc.template.bash
@@ -5,7 +5,7 @@ alias cdzlc="cd $ZEROTHLIFE_CODE_DIR"
 
 alias inaminute="sleep-verbose 60 && echo y && exit 0"
 
-alias forkit="zl-fork-delegate  && echo y && exit 0"
+alias forkit="zl-fork-delegate && echo y && exit 0"
 alias effit="zl-fork-delegate && echo y && exit 0"
 
 alias zgy="zl-gamify && echo y && exit 0"
