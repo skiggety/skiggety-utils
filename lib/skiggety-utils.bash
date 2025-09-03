24b959d00c93e28d081a094a9cd035f6ffafa0d2
Scott Longley <skiggety@tinhat.local>
2025-09-03 10:37:52 -0400

    just some debug output

--- lib/skiggety-utils.bash
+++ lib/skiggety-utils.bash
@@ -92,9 +92,13 @@ function exit_if_any_accumulated_errors {
 
 function exit_if_n_times_this_already_running {
     threshold="$(( $1 + 1 ))" # +1 to allow for self
+    debug_eval_here threshold
     target="$2"
+    debug_eval_here target
     OTHER_SCRIPT_PSLINES="$( pstree | grep -v grep | grep --color=always "\\/$target" )"
+    debug_eval_here OTHER_SCRIPT_PSLINES
     if [ $( echo "$OTHER_SCRIPT_PSLINES" | wc -l ) -gt "$threshold" ]; then
+        debug_here
         echo
         echo "It seems like there are a bunch of these running already:"
         echo "$OTHER_SCRIPT_PSLINES"
