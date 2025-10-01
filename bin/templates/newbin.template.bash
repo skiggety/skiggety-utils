d257145722c06dbde22a990e0e50de098f5c1916
Scott Longley <skiggety@tinhat.local>
2025-10-01 17:37:54 -0400

    newbin uses a template

---
+++ bin/templates/newbin.template.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# This script (...TODO, describe your script here, and DO_NOTooo_COMMIT until then.)
+
+. $SKIGGETY_UTILS_DIR/lib/skiggety-utils.bash || exit 1
+
+interactive_develop_here 'TODO XXoooX: IMPLEMENT this script'
+exit_with_error 'TODO XXoooX: TEST this script'
+
+pushd $ZEROTHLIFE_CODE_DIR;git add -N $0;popd # XXoooX
