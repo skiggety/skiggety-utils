49da460a413ac953d45a4b1806c96c193d9b4b4d
Scott Longley <skiggety@tinhat.local>
2025-10-06 22:14:39 -0400

    zerothlife setup

--- installers/zerothlife.config/include_in_bashrc.template.bash
+++ installers/zerothlife.config/include_in_bashrc.template.bash
@@ -1,15 +1,34 @@
 export PATH="$ZEROTHLIFE_DIR/bin:$ZEROTHLIFE_CODE_DIR/bin:$PATH"
 
+alias pause_windy_terminals="if zl-shellask wanna groom windows before pause; then zl-groom-terminal-window-locations --multiple 6;fi;zl-manually-reposition-windows"
+alias zl-pause="zl-manually-reposition-windows"
+
 alias cdzl="cd $ZEROTHLIFE_DIR"
 alias cdzlc="cd $ZEROTHLIFE_CODE_DIR"
 
-alias brush='zl-fork "zl-size-terminal -n 8;sleep-verbose --vv 10;zl-groom-terminal-window-locations --multiple 20"'
-alias brushy='zl-fork "zl-size-terminal -n 8;sleep-verbose --vv 10;zl-groom-terminal-window-locations --multiple 20";exit 0'
+alias devall='zl-popup-all-code-dev'
+alias dev_all='zl-popup-all-code-dev'
+
+alias zlf='zl-fork'
+
+alias metapuff='vw zl-puff;puff'
+alias metapuffy='vw zl-puff;puffy'
+
+alias nudge='NUDGE=8 zl-groom-terminal-window-locations --multiple 1'
+alias big_nudge='NUDGE=20 zl-groom-terminal-window-locations --multiple 15'
+alias brush='zl-fork "zl-size-terminal -n 8;sleep-verbose --vv 5;zl-groom-terminal-window-locations --multiple 10;sleep 5"'
+alias brushy='zl-fork "zl-size-terminal -n 8;sleep-verbose --vv 5;zl-groom-terminal-window-locations --multiple 10";exit 0'
+alias brushalot='zl-fork "zl-size-terminal -n 8;zl-groom-terminal-window-locations --multiple 100"'
+alias brush_fully='num=1;while zl-groom-terminal-window-locations --multiple $num | grep --color=always Moved;do sleep $num;echo "--------------------------------------------------------------------------------";num="$(( num + 1 ))";done;zl-groom-terminal-window-locations --multiple $num'
+
+alias zl-ballast-windows='for num in 1 2 3;do zl-fork in_five_minutes ;done'
 
 alias splash='zl-fork zl-groom-terminal-window-locations --multiple 1'
 alias splashy='zl-fork zl-groom-terminal-window-locations --multiple 1;exit 0'
 
 alias inaminute="sleep-verbose 60 && echo y && exit 0"
+alias in_a_minute="sleep-verbose 60 && echo y && exit 0"
+alias in_five_minutes="sleep-verbose 300 && echo y && exit 0"
 
 alias forkit="zl-fork-delegate && echo y && exit 0"
 alias effit="zl-fork-delegate && echo y && exit 0"
