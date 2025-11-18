dba33ee9d9f6f417ba9d5613b1425eb8e6c717cc
Scott Longley <skiggety@Mac.lan>
2025-11-18 10:20:22 -0500

    cleanup

--- bin/templates/newbin.template.bash
+++ bin/templates/newbin.template.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# This script (...TODO, describe your script here, and DO_NOTooo_COMMIT until then.)
+# This script (...TOoooDO, describe your script here, and DO_NOTooo_COMMIT until then.)
 
 . $SKIGGETY_UTILS_DIR/lib/skiggety-utils.bash || exit 1
 
@@ -9,10 +9,12 @@ while [[ "$1" == -* ]]; do
     if [ "$1" == '-x' ] || [ "$1" == '--XoooXX' ]; then
         XoooXX_THINGY='whatever DO_NOT_COMoooMIT'
         shift
+    else
+        exit_with_error "BAD OPTION: $1"
     fi
 done
 
 pushd $(dirname $0);git add -N $0;popd # XXoooX (COPPIPASTA)
 
-interactive_develop_here 'TODO XXoooX: IMPLEMENT this script (COPPIPASTA)'
-exit_with_error 'TODO XXoooX: TEST this script (COPPIPASTA)'
+interactive_develop_here 'TOoooDO XXoooX: IMPLEMENT this script (COPPIPASTA)'
+exit_with_error 'TOoooDO XXoooX: TEST this script (COPPIPASTA)'
